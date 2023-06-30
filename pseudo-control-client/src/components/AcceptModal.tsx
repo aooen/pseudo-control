c36d83ac1e97f53dad6d3c9f3b55e3ba9b94d3a6
Hakyeon Kim <khy@aooen.com>
2023-06-30 21:04:43 +0900

    enhance(client): design accept modal

--- pseudo-control-client/src/components/AcceptModal.tsx
+++ pseudo-control-client/src/components/AcceptModal.tsx
@@ -6,18 +6,22 @@ import styles from './modal.module.scss'
 function AcceptModal() {
   return (
     <React.StrictMode>
-      <div className={styles.modal}>
-        <h1>{ literal.text('accept_modal.title') }</h1>
-        <div>
-          { literal.text('accept_modal.description') }
-        </div>
-        <div>
-          <button onClick={() => interaction.run('accept')}>
-            { literal.text('accept_modal.accept') }
-          </button>
-          <button onClick={() => interaction.run('close')}>
-            { literal.text('accept_modal.decline') }
-          </button>
+      <div className={styles.wrapper}>
+        <div className={styles.modal}>
+          <div className={styles.title}>
+            { literal.text('accept_modal.title') }
+          </div>
+          <div className={styles.description}>
+            { literal.text('accept_modal.description') }
+          </div>
+          <div className={styles.buttons}>
+            <button onClick={() => interaction.run('accept')}>
+              { literal.text('accept_modal.accept') }
+            </button>
+            <button onClick={() => interaction.run('close')}>
+              { literal.text('accept_modal.decline') }
+            </button>
+          </div>
         </div>
       </div>
     </React.StrictMode>
